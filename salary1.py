2df99e595623a375337221b58eb6b9eeebbe7082
ArunNejakar <arunnejakar847@gmail.com>
2025-11-20 22:31:56 +0530

    Update salary1.py

--- salary1.py
+++ salary1.py
@@ -1,11 +1,17 @@
-import sys 
-if len(sys.argv) == 2: 
-salary = sys.argv[1] 
-print("User provided input value:") 
-else: 
-print("No input given - using default value:") 
-salary = "30000"  # default salary 
-bonus = eval(salary) * 0.10 
-total = eval(salary) + bonus 
-print("Bonus Amount:", bonus) 
-print("Total Salary after Bonus:", total) 
+import sys
+
+if len(sys.argv) == 2:
+    salary = sys.argv[1]
+    print("User provided input value:", salary)
+else:
+    print("No input given - using default value:")
+    salary = "30000"  # default salary
+
+# Convert string to number (float)
+salary_value = float(salary)
+
+bonus = salary_value * 0.10
+total = salary_value + bonus
+
+print("Bonus Amount:", bonus)
+print("Total Salary after Bonus:", total)
